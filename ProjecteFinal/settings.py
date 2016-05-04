5054f979a7a24797c033f00a1fa52e07afb0a42e
Andreu <andreurp96@gmail.com>
2016-05-04 19:54:24 +0200

    class ~, templates,usuaris

--- ProjecteFinal/settings.py
+++ ProjecteFinal/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/1.9/ref/settings/
 """
 
 import os
+from django.contrib import messages
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -37,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'crispy_forms',
     'components',
     'usuaris',
 ]
@@ -136,3 +138,14 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/static/'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'fitxers')
+MEDIA_URL = "/media/"
+
+MESSAGE_STORAGE = 'django.contrib.messages.storage.session.SessionStorage'
+MESSAGE_TAGS ={ messages.ERROR:'danger' }
+
+CRISPY_TEMPLATE_PACK = 'bootstrap3'
+
+LOGIN_URL = '/login/'
+LOGOUT_URL = '/logout/'
