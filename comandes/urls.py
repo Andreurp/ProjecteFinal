d9e603b2be96aadefc5117aa5d3710c28d969783
Andreu <andreurp96@gmail.com>
2016-05-12 12:05:57 +0200

    carret compre

--- comandes/urls.py
+++ comandes/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     url(r'^veure_comande/$', views.veure_comande, name='veure_comande'),
 
     url(r'^update_session/(?P<id_producte>[0-9]+)/$', views.update_session, name='update_session'),
+    url(r'^esborra_carro/$', views.esborra_carro, name='esborra_carro'),
 ]
--- comandes/views.py
+++ comandes/views.py
@@ -5,13 +5,20 @@ from django.http import *
 from django.contrib import messages
 from django.core.urlresolvers import reverse
 
+from productes.models import Tipus_Producte
+
 #Afagir Producte
 
 def update_session(request, id_producte):
+
     if 'carro' not in request.session:
-        request.session['carro'] =[id_producte,1]
+        request.session['carro'] ={}
+
+    if id_producte not in request.session['carro']:
+        qtat = 1
     else:
-        request.session['carro'] += [id_producte,1]
+        qtat = request.session['carro'][id_producte]+1
+    request.session['carro'].update({id_producte:qtat})
 
     messages.add_message(request, messages.SUCCESS, 'El producte ha sigut afegit correctament')
     return HttpResponseRedirect(reverse('producte:veure_productes') )
@@ -19,6 +26,14 @@ def update_session(request, id_producte):
 #Comandes
 
 def veure_comande(request):
+    if 'carro' not in request.session:
+        request.session['carro'] = {}
+
+    tipus = Tipus_Producte.objects.all()
     #comandes = Comanda.objects.all()
-    comandes =(request.session['carro'])
-    return render(request, 'comandes/index.html', {'comande': comandes})
+    comandes =request.session['carro']
+    return render(request, 'comandes/index.html', {'comandes': comandes, 'tipus': tipus})
+
+def esborra_carro(request):
+    request.session['carro']={}
+    return HttpResponseRedirect(reverse('producte:veure_productes'))
