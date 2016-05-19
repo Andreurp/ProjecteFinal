c39b61a221f59ab37f294dcff9a490497cbc8e78
Andreu <andreurp96@gmail.com>
2016-05-19 17:19:02 +0200

    llista de comandes, detall comanda, fitxerxs js i css saparats

--- ProjecteFinal/settings.py
+++ ProjecteFinal/settings.py
@@ -139,6 +139,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = ''
+STATICFILES_DIRS = ( os.path.join('static'), )
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'fitxers')
 MEDIA_URL = "/media/"
